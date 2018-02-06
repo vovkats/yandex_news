77af2912c253b193890c496bede8a6cfd74cc00d
Vladimir Tsymbal <vladimirts1991@gmail.com>
2018-02-06 16:48:04 +0500

    implement parsing yandex news

--- app/operations/op/news/parse.rb
+++ app/operations/op/news/parse.rb
@@ -1,18 +1,57 @@
 module Op
   module News
     class Parse
-      attr_reader :source
+      URL = 'https://news.yandex.ru/ru/index5.utf8.js'.freeze
+      TIMEOUT_SECONDS = 10.freeze
+      TYPE = 'yandex'.freeze
 
-      def self.execute(source)
-        new(source).execute
+      TimeoutError = Class.new(StandardError)
+      CorrespondingContentNotFound = Class.new(StandardError)
+
+      def self.execute
+        new.execute
       end
 
-      def initialize(source)
-        @source = source
+      def initialize
+        @errors = []
+        @news = []
       end
 
       def execute
-        @row_data = source.parse
+        @news = parse(data)
+
+        { errors: @errors, news: @news}
+      end
+
+      private
+
+      def parse(data)
+        return [] unless data
+
+        news_string = /\[.+\]/.match(data)
+
+        if news_string && news_string[0]
+          JSON.parse(news_string[0]).map do |yandex_news|
+            {
+              ts: yandex_news['ts'],
+              time: yandex_news['time'],
+              date: yandex_news['date'],
+              title: yandex_news['title'],
+              description: yandex_news['descr']
+            }
+          end
+        else
+          @errors << CorrespondingContentNotFound.new
+          []
+        end
+      end
+
+      def data
+        response = RestClient::Request.execute(method: :get, url: URL, timeout: TIMEOUT_SECONDS)
+        response.body
+      rescue RestClient::Exceptions::Timeout
+        @errors << TimeoutError.new
+        nil
       end
     end
   end
