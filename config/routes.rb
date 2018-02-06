ac28db695f249c93270f0459e284eada135614ec
Vladimir Tsymbal <vladimirts1991@gmail.com>
2018-02-06 18:16:02 +0500

    simple authentication for user(admin)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ require 'sidekiq-scheduler/web'
 
 Rails.application.routes.draw do
 
+  devise_for :users
   mount Sidekiq::Web => '/sidekiq'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
