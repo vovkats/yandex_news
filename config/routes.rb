768459d74a2cf747857b6290c554dc68ebc35257
Vladimir Tsymbal <vladimirts1991@gmail.com>
2018-02-09 14:17:42 +0500

    set root page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   mount Sidekiq::Web => '/sidekiq'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  mount ActionCable.server => '/cable'
+  resource :news, only: :show
+
+  root to: 'news#show'
 end
