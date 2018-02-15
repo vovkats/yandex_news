9da98667b917a325266253d567b4b49814021a62
Vladimir Tsymbal <vladimirts1991@gmail.com>
2018-02-15 21:03:58 +0500

    change grid for vue components

--- app/javascript/components/app.vue
+++ app/javascript/components/app.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
   v-app
-    authors-form
+    v-container
+      authors-form
 </template>
 
 <script>
--- app/javascript/components/authors-form.vue
+++ app/javascript/components/authors-form.vue
@@ -1,108 +1,89 @@
 <template>
-    <div>
-        <v-app>
-            <template>
+  <v-flex xs11 lg6 offset-sm1>
+     <v-form v-model="valid" ref="form" lazy-validation>
+        <v-alert outline
+                color="success"
+                icon="check_circle"
+                v-model="sendData"
+                transition="scale-transition"
+                dismissible>
+          Данные обновлены
+        </v-alert>
+        <v-alert outline
+                color="error"
+                icon="check_circle"
+                v-model="sendFault"
+                transition="scale-transition"
+                dismissible>
+          Проверьте данные
+        </v-alert>
 
-                <v-form v-model="valid" ref="form" lazy-validation>
-                    <v-layout row wrap>
-                        <v-flex xs10 offset-xs1>
-                            <v-alert outline
-                                    color="success"
-                                    icon="check_circle"
-                                    v-model="sendData"
-                                    transition="scale-transition"
-                                    dismissible
-                            >
-                            Данные обновлены
-                            </v-alert>
-                            <v-alert outline
-                                    color="error"
-                                    icon="check_circle"
-                                    v-model="sendFault"
-                                    transition="scale-transition"
-                                    dismissible
-                            >
-                                Проверьте данные
-                            </v-alert>
-                        </v-flex>
-                        <v-flex xs11 lg6 offset-sm1>
+        <v-text-field
+                label="Заголовок"
+                v-model="news.title"
+                :rules="commonRules"
+                :counter="100"
+                prepend-icon="title"
+                required>
+        </v-text-field>
 
-                            <v-text-field
-                                    label="Заголовок"
-                                    v-model="news.title"
-                                    :rules="commonRules"
-                                    :counter="100"
-                                    prepend-icon="title"
-                                    required
-                            ></v-text-field>
+        <v-text-field
+                label="Описание"
+                v-model="news.description"
+                :rules="commonRules"
+                prepend-icon="description"
+                required
+                textarea>
+        </v-text-field>
 
-                            <v-text-field
-                                    label="Описание"
-                                    v-model="news.description"
-                                    :rules="commonRules"
-                                    prepend-icon="description"
-                                    required
-                                    textarea
-                            ></v-text-field>
+        <v-menu lazy
+                v-model="menuDate"
+                transition="scale-transition"
+                offset-y
+                full-width
+                :nudge-right="40"
+                max-width="290px"
+                min-width="290px">
+            <v-text-field
+                    slot="activator"
+                    label="Дата"
+                    v-model="news.date"
+                    prepend-icon="date_range"
+                    required
+                    readonly>
+            </v-text-field>
+            <v-date-picker locale="ru-Latn" v-model="news.date"></v-date-picker>
+        </v-menu>
 
-                            <v-menu
-                                    lazy
-                                    v-model="menuDate"
-                                    transition="scale-transition"
-                                    offset-y
-                                    full-width
-                                    :nudge-right="40"
-                                    max-width="290px"
-                                    min-width="290px"
-                            >
-                                <v-text-field
-                                        slot="activator"
-                                        label="Дата"
-                                        v-model="news.date"
-                                        prepend-icon="date_range"
-                                        required
-                                        readonly
-                                ></v-text-field>
-                                <v-date-picker locale="ru-Latn" v-model="news.date"></v-date-picker>
-                            </v-menu>
+        <v-menu :close-on-content-click="false"
+                lazy
+                v-model="menuTime"
+                transition="scale-transition"
+                offset-y
+                full-width
+                :nudge-right="40"
+                max-width="290px"
+                min-width="290px">
+            <v-text-field slot="activator"
+                    label="Время"
+                    v-model="news.time"
+                    prepend-icon="access_time"
+                    required
+                    readonly>
+            </v-text-field>
+            <v-time-picker v-model="news.time" autosave format="24hr"></v-time-picker>
+        </v-menu>
 
-                            <v-menu
-                                    :close-on-content-click="false"
-                                    lazy
-                                    v-model="menuTime"
-                                    transition="scale-transition"
-                                    offset-y
-                                    full-width
-                                    :nudge-right="40"
-                                    max-width="290px"
-                                    min-width="290px"
-                            >
-                                <v-text-field
-                                        slot="activator"
-                                        label="Время"
-                                        v-model="news.time"
-                                        prepend-icon="access_time"
-                                        required
-                                        readonly
-                                ></v-text-field>
-                                <v-time-picker v-model="news.time" autosave format="24hr"></v-time-picker>
-                            </v-menu>
-
-                            <v-btn @click="submit" :disabled="!valid">
-                                <span v-if="persistedNews">
-                                    Редактировать
-                                </span >
-
-                                <span v-else>
-                                    Создать
-                                </span >
-                            </v-btn>
-                        </v-flex>
-                    </v-layout>
-                </v-form>
-            </template>
-        </v-app>
-    </div>
+        <v-btn @click="submit" :disabled="!valid">
+            <span v-if="persistedNews">
+                Редактировать
+            </span>
+            <span v-else>
+                Создать
+            </span>
+        </v-btn>
+     </v-form>
+  </v-flex>
 </template>
 
 <script>
